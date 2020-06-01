1eefd275aff7154f714365b0eb9ad2f02462ff58
Michael Kharchenko <mihail.macnamara@gmail.com>
2020-06-01 16:23:08 +0300

    111

--- index.php
+++ index.php
@@ -6,35 +6,43 @@ use Amp\Http\Client\Request;
 use Amp\Loop;
 use Symfony\Component\DomCrawler\Crawler;
 
-$from = new DateTime('2006-06-01');
+$from = new DateTime('2006-01-01');
 $to = new DateTime('2006-12-31');
 
 
 Loop::run(function () use (&$from, $to) {
     $client = HttpClientBuilder::buildDefault();
     $handle = yield \Amp\File\open($from->format("Y-m-d") . "_" . $to->format("Y-m-d") . ".txt", "w");
+    $log = yield \Amp\File\open($from->format("Y-m-d") . "_" . $to->format("Y-m-d") . "log" . ".txt", "w");
 
     while ($from < $to) {
         $uri = "https://whoistory.com/" . $from->format('/Y/m/d/');
         $response = yield $client->request(new Request($uri, 'GET'));
 
-        if ($response->getStatus() == 200){
+        if ($response->getStatus() == 200) {
             $crawler = new Crawler((string) yield $response->getBody()->buffer());
             $links = $crawler->filter('div.left > a');
-            foreach ($links as $link) {
-                try {
-                    $request = new Request("http://" . $link->textContent);
-                    $request->setTcpConnectTimeout(3000);
-                    $domain = yield $client->request($request);
-                    if ($domain->getStatus() == 200 || $domain->getStatus() == 302) {
-                        $handle->write($link->textContent . "\n");
-                        echo $link->textContent . " : 200" . PHP_EOL;
+            $valid = 0;
+
+            if (count($links) > 1) {
+                foreach ($links as $link) {
+                    try {
+                        $request = new Request("http://" . $link->textContent);
+                        $request->setTcpConnectTimeout(3000);
+                        $domain = yield $client->request($request);
+                        if ($domain->getStatus() == 200 || $domain->getStatus() == 302) {
+                            $handle->write($link->textContent . "\n");
+                            $valid += 1;
+                            echo $link->textContent . " : 200" . PHP_EOL;
+                        }
+                    } catch (Exception $e) {
+                        echo $e->getCode() . " : " . $e->getMessage() . PHP_EOL;
                     }
-                } catch (Exception $e) {
-                    echo $e->getCode() . " : " . $e->getMessage() . PHP_EOL;
                 }
             }
-            echo $uri . ' : ' . $links->count() . PHP_EOL;
+            $log->write($uri . " domains: {$links->count()} , valid: {$valid};" . PHP_EOL);
+            echo $uri . ' : ' . $links->count() . ", valid: {$valid};" . PHP_EOL;
+            $valid = 0;
         } else {
             echo $uri . " 404" . PHP_EOL;
         }
